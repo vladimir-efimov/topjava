97904889309d097058ea50f2a28ba64cdef00e88
Vladimir Efimov <vladimir@localhost>
2017-05-09 17:19:45 +0700

    Fixed work with cache for JDBC

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Before;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
@@ -7,4 +8,10 @@ import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+        service.evictCache();
+    }
+
 }
