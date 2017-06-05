ac36bbd18f1bed10c0b45473dbcb85f51442d547
Vladimir Efimov <vladimir@localhost>
2017-06-06 01:24:43 +0700

    Applying patch "11 0 fix"

--- src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
@@ -37,7 +37,7 @@ public class AdminRestControllerTest extends AbstractControllerTest {
     @Test
     public void testGetNotFound() throws Exception {
         mockMvc.perform(get(REST_URL + 1)
-                .with(TestUtil.userHttpBasic(ADMIN)))
+                .with(userHttpBasic(ADMIN)))
                 .andExpect(status().isUnprocessableEntity())
                 .andDo(print());
     }
@@ -63,7 +63,7 @@ public class AdminRestControllerTest extends AbstractControllerTest {
     @Test
     public void testDeleteNotFound() throws Exception {
         mockMvc.perform(delete(REST_URL + 1)
-                .with(TestUtil.userHttpBasic(ADMIN)))
+                .with(userHttpBasic(ADMIN)))
                 .andExpect(status().isUnprocessableEntity())
                 .andDo(print());
     }
