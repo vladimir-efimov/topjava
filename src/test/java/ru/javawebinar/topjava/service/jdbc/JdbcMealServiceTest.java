e45494e45e0de284c6fd5fc57780ee509556d43d
Vladimir Efimov <vladimir.a.efimov@gmail.com>
2024-11-18 19:05:35 +0700

    Ignore test createWithException

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,13 +1,17 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+import org.junit.Ignore;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
     @Override
+    @Ignore
+    @Test
     public void createWithException() throws Exception {
     }
 }
